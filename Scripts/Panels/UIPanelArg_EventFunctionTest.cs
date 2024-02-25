01180398f0eeba28996a8170a4af8b869e307a07
DE-YU <delsin_yu@qq.com>
2024-02-26 00:06:56 +0800

    Adds token related tests

--- Scripts/Panels/UIPanelArg_EventFunctionTest.cs
+++ Scripts/Panels/UIPanelArg_EventFunctionTest.cs
@@ -3,10 +3,12 @@ using GodotTask;
 
 namespace GDPanelFramework.Tests;
 
-public partial class UIPanel_EventFunctionTest : UIPanel
+public partial class UIPanelArg_EventFunctionTest : UIPanelArg<int, int>
 {
     public class TestMonitor
     {
+        public int OpenValue { get; set; }
+        public int CloseValue { get; set; }
         public bool Initialized { get; set; }
         public bool Opened { get; set; }
         public bool Closed { get; set; }
@@ -18,13 +20,20 @@ public partial class UIPanel_EventFunctionTest : UIPanel
     
     protected override void _OnPanelInitialize() => Monitor.Initialized = true;
 
-    protected override void _OnPanelOpen()
+    protected override void _OnPanelOpen(int value)
     {
         Monitor.Opened = true;
-        GDTask.NextFrame().ContinueWith(ClosePanel);
+        Monitor.OpenValue = value;
+        GDTask.NextFrame().ContinueWith(ClosePanelWithValue);
     }
 
-    protected override void _OnPanelClose() => Monitor.Closed = true;
+    private void ClosePanelWithValue() => ClosePanel(OpenArg * 2);
+
+    protected override void _OnPanelClose(int value)
+    {
+        Monitor.Closed = true;
+        Monitor.CloseValue = value;
+    }
 
     protected override void _OnPanelPredelete() => Monitor.Predelete = true;
 
@@ -32,5 +41,4 @@ public partial class UIPanel_EventFunctionTest : UIPanel
     {
         if (Monitor != null) Monitor.Notification = true;
     }
-
 }
--- Scripts/test/GDPanelFrameworkTest_UIPanel.cs
+++ Scripts/test/GDPanelFrameworkTest_UIPanel.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Diagnostics;
 using GdUnit4;
 using System.Threading.Tasks;
-using GdUnit4.Exceptions;
 using Godot;
 using GodotTask;
 
@@ -11,11 +8,17 @@ namespace GDPanelFramework.Tests;
 [TestSuite]
 public class GDPanelFrameworkTest_UIPanel
 {
+    // [TestCase]
+    // public static async Task WaitForDebugger()
+    // {
+    //     await GDTask.WaitUntil(() => System.Diagnostics.Debugger.IsAttached);
+    // }
+
     [TestCase]
-    public static async Task UIPanel_EventFunction()
+    public static async Task UIPanel_Test_EventFunction()
     {
         await GDTask.NextFrame();
-        
+
         var resource = GD.Load<PackedScene>("res://Prefabs/UIPanel_EventFunctionTest.tscn");
 
         var monitor = new UIPanel_EventFunctionTest.TestMonitor();
@@ -25,11 +28,79 @@ public class GDPanelFrameworkTest_UIPanel
             .OpenPanelAsync(closePolicy: ClosePolicy.Delete);
 
         await GDTask.NextFrame();
-        
+
         Assertions.AssertThat(monitor.Initialized).IsTrue();
         Assertions.AssertThat(monitor.Opened).IsTrue();
         Assertions.AssertThat(monitor.Closed).IsTrue();
         Assertions.AssertThat(monitor.Predelete).IsTrue();
         Assertions.AssertThat(monitor.Notification).IsTrue();
+    }
+
+    [TestCase]
+    public static async Task UIPanelArg_Test_EventFunction()
+    {
+        var resource = GD.Load<PackedScene>("res://Prefabs/UIPanelArg_EventFunctionTest.tscn");
+
+        var monitor = new UIPanelArg_EventFunctionTest.TestMonitor();
+
+        const int openArg = 10;
+
+        var closeValue = await resource
+            .CreatePanel<UIPanelArg_EventFunctionTest>(initializeCallback: panel => panel.Monitor = monitor)
+            .OpenPanelAsync(openArg, closePolicy: ClosePolicy.Delete);
+
+        await GDTask.NextFrame();
+
+        Assertions.AssertThat(monitor.Initialized).IsTrue();
+        Assertions.AssertThat(monitor.Opened).IsTrue();
+        Assertions.AssertThat(monitor.Closed).IsTrue();
+        Assertions.AssertThat(monitor.Predelete).IsTrue();
+        Assertions.AssertThat(monitor.Notification).IsTrue();
+
+        Assertions.AssertThat(monitor.OpenValue).IsEqual(openArg);
+        Assertions.AssertThat(monitor.CloseValue).IsEqual(openArg * 2);
+        Assertions.AssertThat(monitor.CloseValue).IsEqual(closeValue);
+    }
+
+    [TestCase]
+    public static async Task UIPanel_Test_Token()
+    {
+        await GDTask.NextFrame();
+
+        var resource = GD.Load<PackedScene>("res://Prefabs/UIPanel_TokenTest.tscn");
+
+        var monitor = new UIPanel_TokenTest.TestMonitor();
+
+        await resource
+            .CreatePanel<UIPanel_TokenTest>(initializeCallback: panel => panel.Monitor = monitor)
+            .OpenPanelAsync(closePolicy: ClosePolicy.Delete);
+
+        await GDTask.NextFrame();
+
+        Assertions.AssertThat(monitor.PanelCloseTokenCanceled).IsTrue();
+        Assertions.AssertThat(monitor.PanelCloseTweenFinishTokenCanceled).IsTrue();
+        Assertions.AssertThat(monitor.PanelOpenTweenFinishTokenCanceled).IsTrue();
+    }
+
+    [TestCase]
+    public static async Task UIPanelArg_Test_Token()
+    {
+        var resource = GD.Load<PackedScene>("res://Prefabs/UIPanelArg_TokenTest.tscn");
+
+        var monitor = new UIPanelArg_TokenTest.TestMonitor();
+
+        const int openArg = 10;
+
+        var closeValue = await resource
+            .CreatePanel<UIPanelArg_TokenTest>(initializeCallback: panel => panel.Monitor = monitor)
+            .OpenPanelAsync(openArg, closePolicy: ClosePolicy.Delete);
+
+        await GDTask.NextFrame();
+
+        Assertions.AssertThat(closeValue).IsEqual(closeValue);
+        
+        Assertions.AssertThat(monitor.PanelCloseTokenCanceled).IsTrue();
+        Assertions.AssertThat(monitor.PanelCloseTweenFinishTokenCanceled).IsTrue();
+        Assertions.AssertThat(monitor.PanelOpenTweenFinishTokenCanceled).IsTrue();
     }
 }
